596e3c91bb5ed39ce3e24336e854162fe85020d1
Tom Lee <tomlee.tomlee@gmail.com>
2014-10-20 19:43:10 -0700

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Tom Lee on 10/20/14.
+//  Copyright (c) 2014 Tom Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Tom Lee on 10/20/14.
+//  Copyright (c) 2014 Tom Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
